d6c94ee0b4d37641a7978836c86eefe9728d4e1e
levan <levan@galleon.so>
2026-02-03 17:50:17 +0200

    Adds payment method definition properties
    Adds new properties to the PaymentMethodDefinitionData class.
    
    These properties include display name, display options for empty
    unified payment methods, and the display mode for selection page
    unified payment methods.

--- CheckoutSharedModels.cs
+++ CheckoutSharedModels.cs
@@ -154,16 +154,21 @@ namespace Galleon.Checkout.Shared
     [Serializable]
     public partial class PaymentMethodDefinitionData
     {
-        public string                type                   = "";
+        public string                type                               = "";
         
-        public List<PaymentProvider> providers              = new List<PaymentProvider>();
+        public string                display_name                       = "";
         
-        public string                icon_url               = "";
-        public string                logo_url               = "";
+        public bool                  display_empty_upm_in_checkout_page = false;
+        public string                selection_page_upms_display_mode   = "default";
         
-        public List<PaymentAction>   initialization_actions = new List<PaymentAction>();
-        public List<PaymentAction>   vaulting_actions       = new List<PaymentAction>();
-        public List<PaymentAction>   charge_actions         = new List<PaymentAction>();
+        public List<PaymentProvider> providers                          = new List<PaymentProvider>();
+        
+        public string                icon_url                           = "";
+        public string                logo_url                           = "";
+        
+        public List<PaymentAction>   initialization_actions             = new List<PaymentAction>();
+        public List<PaymentAction>   vaulting_actions                   = new List<PaymentAction>();
+        public List<PaymentAction>   charge_actions                     = new List<PaymentAction>();
     }
 
     [Serializable]
